89b277be4bdb8bfeccd9f172b70b0ded6c317ae7
AteebHussain2 <student2ateeb.hussain@gmail.com>
2026-07-13 19:02:43 +0500

    feat: added cors origins for production

--- backend/api-gateway/src/lib/origins.ts
+++ backend/api-gateway/src/lib/origins.ts
@@ -2,8 +2,17 @@
 // all other requests from anyone else except
 // the mentioned origins will be denied
 
-export const allowedOrigins = [
+const devOrigins = [
     "http://localhost:3000",
-    "http://localhost:80",
+    "http://localhost:81",
     "http://localhost",
-]
+];
+
+const prodOrigins = [
+    "https://mafia.markaz.network",
+    "https://home.markaz.network",
+];
+
+export const allowedOrigins = process.env.NODE_ENV === "production"
+    ? prodOrigins
+    : devOrigins;
