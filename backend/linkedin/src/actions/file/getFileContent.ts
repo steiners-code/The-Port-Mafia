fdc93124d96b84ecb2ac9eb2ff1bcb943a28bcc2
AteebHussain2 <student2ateeb.hussain@gmail.com>
2026-08-29 17:07:10 +0500

    fix: empty file display error

--- backend/linkedin/src/actions/file/getFileContent.ts
+++ backend/linkedin/src/actions/file/getFileContent.ts
@@ -3,8 +3,10 @@ import { prisma } from "../../lib/db";
 
 export async function getFileContent(userId: string, fileType: File["fileType"]) {
     try {
-        const data = await prisma.linkedinFile.findUnique({
+        const data = await prisma.linkedinFile.upsert({
             where: { userId_fileType: { userId, fileType } },
+            create: { userId, fileType, content: "" },
+            update: {},
             select: { content: true }
         })
 
